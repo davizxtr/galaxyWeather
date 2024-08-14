45bc11f6d8f69ae5128b47d94e75e1ec88ca7449
David <daviz_xtr@hotmail.com>
2024-08-14 23:27:36 +0200

    correccion estilos header

--- components/HeaderApp/HeaderApp.vue
+++ components/HeaderApp/HeaderApp.vue
@@ -4,7 +4,7 @@
     </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
 header {
     background-color: white;
     font-size: 1.5rem;
@@ -13,5 +13,9 @@ header {
     font-weight: 400;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     margin-bottom: 30px;
+    padding: 20px 0;
+    p {
+        margin: 0;
+    }
 }
 </style>
